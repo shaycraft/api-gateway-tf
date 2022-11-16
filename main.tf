881161dd30fc1b50726eb3b31cb2661ad38ad188
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2022-11-15 20:51:09 -0700

    chore: vcs mappings

--- main.tf
+++ main.tf
@@ -2,21 +2,21 @@ provider "aws" {
     region = "us-west-2"
 }
 
-resource "aws_api_gateway_rest_api" "example" {
+resource "aws_api_gateway_rest_api" "api_gateway_example_fool" {
   body = jsonencode({
     openapi = "3.0.1"
     info = {
-      title   = "example"
+      title   = "api_gateway_example_fool"
       version = "1.0"
     }
     paths = {
-      "/path1" = {
+      "/wtf" = {
         get = {
           x-amazon-apigateway-integration = {
             httpMethod           = "GET"
             payloadFormatVersion = "1.0"
             type                 = "HTTP_PROXY"
-            uri                  = "https://ip-ranges.amazonaws.com/ip-ranges.json"
+            uri                  = "https://mvc.samhaycraft.net"
           }
         }
       }
