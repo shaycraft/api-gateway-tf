b947f7496f2f23b88e37edbf8997a2a9413a0d50
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2022-11-15 21:18:23 -0700

    feat:  updates description field

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ resource "aws_api_gateway_rest_api" "api_gateway_example_fool" {
     info = {
       title   = "api_gateway_example_fool"
       version = "1.0"
+      description = "I am the terraform robot beep beep!"
     }
     paths = {
       "/wtf" = {
