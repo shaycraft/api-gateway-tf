2f145858e23e08f86b3cfd8325b10da8a62295ce
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2022-12-02 22:04:10 -0700

    feat: converts to api v2 and with http proxy parameters

--- main.tf
+++ main.tf
@@ -1,32 +1,42 @@
 provider "aws" {
-    region = "us-west-2"
+  region = "us-west-2"
 }
 
-resource "aws_api_gateway_rest_api" "api_gateway_example_fool" {
+resource "aws_apigatewayv2_api" "api_gateway_example_fool" {
   body = jsonencode({
     openapi = "3.0.1"
     info = {
-      title   = "api_gateway_example_fool"
-      version = "1.0"
+      title       = "api-gateway-example-fool"
+      version     = "1.0"
       description = "I am the terraform robot beep beep!"
     }
     paths = {
-      "/v1" = {
+      "/{proxy+}" = {
+        parameters = {
+          "proxy" = {
+            description = "Path parameter for proxy+"
+            name        = "proxy"
+            in          = "path"
+            required    = true
+            schema = {
+              type = "string"
+            }
+          }
+        },
         get = {
           x-amazon-apigateway-integration = {
             httpMethod           = "GET"
             payloadFormatVersion = "1.0"
             type                 = "HTTP_PROXY"
-            uri                  = "http://mvc.samhaycraft.net/v1"
+            uri                  = "http://mvc.samhaycraft.net/{proxy}"
+            connectionType       = "INTERNET"
           }
         }
       }
     }
   })
 
-  name = "api_gateway_example_fool"
-
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
+  name = "api-gateway-example-fool"
+  protocol_type = "HTTP"
+  
 }
