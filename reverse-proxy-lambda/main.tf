b0bc18fa3e66262f71e2bf787f2291731e87cc61
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2022-12-04 22:03:37 -0700

    refactor: variable renames

--- reverse-proxy-lambda/main.tf
+++ reverse-proxy-lambda/main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_iam_role" "tf_iam_for_lambda" {
-  name = "tf_iam_for_lambda"
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
 
   assume_role_policy = <<EOF
 {
@@ -24,10 +24,10 @@ EOF
 
 
 
-resource "aws_lambda_function" "tf-reverse-proxy-py" {
+resource "aws_lambda_function" "lambda_function" {
   filename      = "lambda_payload.zip"
-  function_name = "tf-reverse-proxy-py"
-  role          = aws_iam_role.tf_iam_for_lambda.arn
+  function_name = "reverse-proxy-arcgis-tf"
+  role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
 }
@@ -36,7 +36,7 @@ resource "aws_apigatewayv2_deployment" "deployment" {
   api_id      = aws_apigatewayv2_api.lambda_proxy_api.id
   description = "Terraform robot deployment beep boop beep!"
   depends_on = [
-    aws_apigatewayv2_route.default_route
+    aws_apigatewayv2_route.proxy_route
   ]
 
   lifecycle {
@@ -50,7 +50,7 @@ resource "aws_apigatewayv2_stage" "stage" {
   deployment_id = aws_apigatewayv2_deployment.deployment.id
 }
 
-resource "aws_apigatewayv2_route" "default_route" {
+resource "aws_apigatewayv2_route" "proxy_route" {
   api_id    = aws_apigatewayv2_api.lambda_proxy_api.id
   route_key = "ANY /{proxy+}"
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
@@ -62,15 +62,16 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
   integration_type = "AWS_PROXY"
 
   connection_type    = "INTERNET"
-  description        = "Lambda proxy integration"
   integration_method = "POST"
-  integration_uri    = aws_lambda_function.tf-reverse-proxy-py.invoke_arn
+  # integration_uri    = aws_lambda_function.lambda_function.invoke_arn
+  integration_uri = aws_lambda_function.lambda_function.arn
+  payload_format_version = "2.0"
 
 }
 
 
 resource "aws_apigatewayv2_api" "lambda_proxy_api" {
-  name          = "Terraform Reverse Proxy Lambda"
+  name          = "terraform-reverse-proxy-lambda"
   protocol_type = "HTTP"
 
 }
