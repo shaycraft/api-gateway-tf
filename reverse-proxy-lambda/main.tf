b20b18826602a17e22f37cec583868728dbbef76
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2022-12-13 16:04:01 -0700

    chore: adding tags/description

--- reverse-proxy-lambda/main.tf
+++ reverse-proxy-lambda/main.tf
@@ -131,6 +131,7 @@ resource "aws_lambda_permission" "apigw_lambda" {
 resource "aws_lambda_function" "lambda_function" {
   filename         = "lambda_payload.zip"
   function_name    = "reverse-proxy-arcgis-tf"
+  description      = "Reverse proxy"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.9"
@@ -138,6 +139,10 @@ resource "aws_lambda_function" "lambda_function" {
   vpc_config {
     subnet_ids         = [aws_subnet.subnet_public.id]
     security_group_ids = [aws_security_group.security_group.id]
+  }
+
+  tags = {
+    Name = "terraform lambda"
   }
 }
 
