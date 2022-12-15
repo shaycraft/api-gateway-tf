7d4d555d4bc64df2f467eb9a18c5837109d96928
Sam Haycraft <phreakdawg@gmail.com>
2022-12-14 20:55:24 -0700

    feat: new lambda source code hash, api gateway tags

--- reverse-proxy-lambda/main.tf
+++ reverse-proxy-lambda/main.tf
@@ -134,13 +134,14 @@ resource "aws_lambda_permission" "apigw_lambda" {
 }
 
 resource "aws_lambda_function" "lambda_function" {
-  filename         = "lambda_payload.zip"
-  function_name    = "small-test"
-  description      = "Reverse proxy"
-  role             = aws_iam_role.iam_for_lambda.arn
-  handler          = var.LAMBDA_HANDLER
-  runtime          = var.LAMBDA_RUNTIME
-  source_code_hash = filebase64("./lambda_payload.zip")
+  filename      = "lambda_payload.zip"
+  function_name = "small-test"
+  description   = "Reverse proxy"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = var.LAMBDA_HANDLER
+  runtime       = var.LAMBDA_RUNTIME
+  #source_code_hash = filebase64("./lambda_payload.zip")
+  source_code_hash = base64sha256(file("./lambda_payload.zip"))
   vpc_config {
     subnet_ids         = [aws_subnet.subnet_public.id]
     security_group_ids = [aws_security_group.security_group.id]
@@ -154,7 +155,7 @@ resource "aws_lambda_function" "lambda_function" {
 resource "aws_apigatewayv2_deployment" "deployment" {
   api_id      = aws_apigatewayv2_api.lambda_proxy_api.id
   description = "Terraform robot deployment beep boop beep!"
-  depends_on  = [
+  depends_on = [
     aws_apigatewayv2_route.proxy_route,
     aws_apigatewayv2_integration.lambda_integration
   ]
@@ -193,14 +194,15 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
   integration_method     = "POST"
   payload_format_version = "2.0"
   integration_uri        = aws_lambda_function.lambda_function.arn
-  # integration_uri        = "https://sampleserver6.arcgisonline.com/{proxy}"
-
-
 }
 
 
 resource "aws_apigatewayv2_api" "lambda_proxy_api" {
   name          = "terraform-reverse-proxy-lambda"
   protocol_type = "HTTP"
+
+  tags = {
+    "Name" = "terraform proxy"
+  }
 
 }
